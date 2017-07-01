6a4ad99215349bd8ac7fdffdca7bef74597a0bbd
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-07-01 21:16:35 +0300

    base page and about page

--- blog/homepage/views.py
+++ blog/homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'homepage/index.html')
+
+
+def about(request):
+    return render(request, 'homepage/about.html')
