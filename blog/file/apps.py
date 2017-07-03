61076c60bf3487792cd9685c6c8435412b57a678
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-07-03 21:48:05 +0300

    fix admin icon dashbord

--- blog/file/apps.py
+++ blog/file/apps.py
@@ -2,6 +2,12 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
+from material.frontend.apps import ModuleMixin
 
-class FileConfig(AppConfig):
+
+class FileConfig(ModuleMixin, AppConfig):
     name = 'file'
+    label = "file"
+
+    verbose_name = "Files"
+    icon = '<i class="material-icons">description</i>'
