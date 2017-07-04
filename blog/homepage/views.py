4abe52a7b8d6f9a3d55331a5511c32d8333e166f
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-07-04 13:53:32 +0300

    google maps in event page

--- blog/homepage/views.py
+++ blog/homepage/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from activity.models import Activity
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html')
+    activities = Activity.objects.all()[:3]
+    return render(request, 'homepage/index.html', {
+        'activities': activities
+    })
 
 
 def about(request):
     return render(request, 'homepage/about.html')
-
-
